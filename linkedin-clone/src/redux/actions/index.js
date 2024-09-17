eb12e9bf89c221084f214ad4681c6493e056e6f1
Carmy93 <carmygargiulo45@gmail.com>
2024-09-17 17:03:43 +0200

    up

--- linkedin-clone/src/redux/actions/index.js
+++ linkedin-clone/src/redux/actions/index.js
@@ -7,6 +7,7 @@ export const RETRIEVE_SEARCH = 'RETRIEVE SEARCH'
 export const SET_SELECTED = 'SET_SELECTED'
 export const RETRIEVE_COMMENTS = 'RETRIEVE_COMMENTS'
 export const SET_TOKEN = 'SET_TOKEN'
+export const RETRIEVE_PROFILE = 'RETRIEVE_PROFILE'
 
 
 export const RetrieveUserAction = (token) => {
@@ -155,5 +156,29 @@ export const SetTokenAction = (token) => {
     return {
         type: SET_TOKEN,
         payload: token
+    }
+}
+
+export const RetrieveProfileAction = (token, id) => {
+    return async (dispatch) => {
+        try {
+            const response = await fetch(`https://striveschool-api.herokuapp.com/api/profile/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            if (response.ok) {
+                const user = await response.json()
+                console.log(user)
+                dispatch({
+                    type: RETRIEVE_PROFILE,
+                    payload: user
+                })
+            } else {
+                throw new Error('ooops')
+            }
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
--- linkedin-clone/src/redux/reducers/user.js
+++ linkedin-clone/src/redux/reducers/user.js
@@ -1,14 +1,20 @@
-import { RETRIEVE_USER } from "../actions";
+import { RETRIEVE_PROFILE, RETRIEVE_USER } from "../actions";
 const initialState = {
-    user_logged: null
+    user_logged: null,
+    profile: null
 }
 
 const userReducer = (state = initialState, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case RETRIEVE_USER:
             return {
                 ...state,
                 user_logged: action.payload
+            }
+        case RETRIEVE_PROFILE:
+            return {
+                ...state,
+                profile: action.payload
             }
         default:
             return state
